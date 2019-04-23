184eb6f62217b616356ea732b81f1f065deee5ca
nexttai <nextsys88@gmail.com>
2019-04-23 18:46:08 +0900

    LeapTest
    Recordできない

---
+++ Assets/Exercise.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Exercise :MonoBehaviour
+{
+
+    public string exerciseName;
+    public Sprite[] teacherFrames;
+    public TextAsset recording;
+    public float score;
+    public Object FontObject;
+}
---
+++ Assets/LeapManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class LeapManager : MonoBehaviour
+{
+    public Exercise exercise;
+    public LeapRecorder leap;
+    public HandModelManager handModel;
+    //public int test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // leap=gameObject.
+        leap = handModel.GetLeapRecorder();
+        if (leap == null)
+        {
+            Debug.Log("なし");
+        }
+        leap.Load(exercise.recording);
+        if (leap == null)
+        {
+            Debug.Log("なし");
+        }
+        else Debug.Log("あり");
+        if (exercise.recording == null)
+        {
+            Debug.Log("jsonファイルなし");
+        }
+        else Debug.Log("jsonファイルあり");
+        Debug.Log(leap.GetFramesCount());
+    }
+    public  void RecordTest()
+    {
+        var newFrames = leap.GetFrames();
+
+
+    }
+    public void UpdateGesture()
+    {
+        leap.Load(exercise.recording);
+        if (leap == null)
+        {
+            Debug.Log("なし");
+        }
+        else Debug.Log("あり");
+        if (exercise.recording == null)
+        {
+            Debug.Log("jsonファイルなし");
+        }
+        else Debug.Log("jsonファイルあり");
+        Debug.Log(leap.GetFramesCount());
+        var newFrames = leap.GetFrames();
+
+        //フレームの最初と最後あたりを除いたものをLeapRecorderに追加
+
+        newFrames.RemoveRange(leap.GetFramesCount() - 200, 199);
+
+        leap.Reset();
+
+        foreach (var f in newFrames)
+            leap.AddFrame(f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //for(int i = 0; i < 5; i++)
+        //{
+        //    test += i;
+        //}
+    }
+}
